df6e1624e947a77316c60bc30a7a6cc1d1b66fc7
typenoob <61347081+typenoob@users.noreply.github.com>
2022-05-17 18:23:35 +0800

    Update Auto-Choose.js

--- Auto-Choose.js
+++ Auto-Choose.js
@@ -1,18 +1,19 @@
 /*
     @Author  coyote
-    @Date    2021-12-24
+    @Date    2022-5-17
     @GitHub  https://github.com/typenoob
 */
 
 
 chart=document.querySelector("#tempGrid").children[0].children
 for(let k=1;k<chart.length;k++){
-    setTimeout(function(){chart[k].click()},k*2000)  
-    setTimeout(function(){a=document.getElementsByClassName('radio-inline')},k*2000+1000)
+    setTimeout(function(){chart[k].click()},k*3000)  
+    setTimeout(function(){a=document.getElementsByClassName('radio-inline')},k*3000+1000)
     for(let i=0;i<40;i++)
-        setTimeout(function(){if(a[i].innerText=='非常满意 ') a[i].children[0].firstElementChild.click()},k*2000+1150)
-    setTimeout(function(){document.querySelector("#btn_xspj_tj").click()},k*2000+1160)
-    setTimeout(function(){document.querySelector("#btn_ok").click()},k*2000+1500)
+   setTimeout(function(){if(a[i].innerText=='非常满意 ') a[i].children[0].firstElementChild.click()},k*3000+1150)
+   setTimeout(function(){document.querySelector("#btn_xspj_tj").dispatchEvent( new Event( 'mouseover' ) )},k*3000+1160)
+   setTimeout(function(){document.querySelector("#btn_xspj_tj").click()},k*3000+1360)
+   setTimeout(function(){document.querySelector("#btn_ok").click()},k*3000+1500)
 }
 
 
